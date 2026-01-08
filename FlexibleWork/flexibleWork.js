4ac40403407602f794e93f539368879a2bc927d2
Sunyoung Lee <s3u0n4@gmail.com>
2026-01-08 20:12:51 +0900

    유연근무제

---
+++ FlexibleWork/flexibleWork.js
@@ -0,0 +1,27 @@
+function solution(schedules, timelogs, startday) {
+  let answer = 0;
+  for (let i = 0; i < schedules.length; i++) {
+    let goal = schedules[i] + 10;
+    if (goal % 100 >= 60) {
+      goal += 100;
+      goal -= 60;
+    }
+
+    let cnt = 0;
+    for (let j = 0; j < 7; j++) {
+      let ref = j + startday;
+      if (ref % 7 === 0 || ref % 7 === 6) {
+        continue;
+      }
+      if (goal < timelogs[i][j]) {
+        break;
+      }
+      ++cnt;
+    }
+
+    if (cnt === 5) {
+      ++answer;
+    }
+  }
+  return answer;
+}
--- PakageBox/pakageBox.js
+++ PakageBox/pakageBox.js
@@ -5,8 +5,8 @@ function solution(n, w, num) {
   const nRemain = n % w || n; // 마지막 줄에 남아있는 상자 개수
   const numRemain = num % w || num; // num이 줄에서 서 있는 위치
 
-  if (total % 2 === target % 2 && nRemain < numRemain) res -= 1;
-  if (total % 2 !== target % 2 && nRemain + numRemain <= w) res -= 1;
+  if (total % 2 === target % 2 && nRemain < numRemain) res -= 1; //같은 방향일 때 마지막 층이 타겟 열까지 채우지 못함
+  if (total % 2 !== target % 2 && nRemain + numRemain <= w) res -= 1; //다른 방향일 때 층이 타겟 열까지 채우지 못함
 
   return total - target + res;
 }
